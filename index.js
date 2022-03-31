988520c48cc8646611e2e37032d578284ac79600
sk <kshen@alibaba-inc.com>
2022-03-31 20:30:12 +0800

    u

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const app = require('express')();
 const wsInstance = require('express-ws')(app);
+const port = process.env.PORT || 3000;
 
 app.ws('/s', ws => {
-	ws.on('message', data => {
-		wsInstance.getWss().clients.forEach(client => {
-			if (client !== ws) {
-				const obj = JSON.parse(data);
-				if (obj.type !== 'heartbeat') client.send(data);
-			}
-		});
-	});
+  ws.on('message', data => {
+    wsInstance.getWss().clients.forEach(client => {
+      if (client !== ws) {
+        const obj = JSON.parse(data);
+        if (obj.type !== 'heartbeat') client.send(data);
+      }
+    });
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
